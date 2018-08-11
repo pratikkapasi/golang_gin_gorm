54bc345e7cf9a99540fd83cc2f202734efa6f23b
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-08-12 01:44:24 +0530

    Read all

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
-
-import "github.com/gin-gonic/gin"
-
+import (
+ "fmt"
+ "github.com/gin-gonic/gin"
+ "github.com/jinzhu/gorm"
+ _ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+var db *gorm.DB
+var err error
+type Person struct {
+ ID uint `json:"id"`
+ FirstName string `json:"firstname"`
+ LastName string `json:"lastname"`
+}
 func main() {
+ // NOTE: See we’re using = to assign the global var
+ // instead of := which would assign it only in this function
+ db, err = gorm.Open("sqlite3", "./gorm.db")
+ if err != nil {
+   fmt.Println(err)
+ }
+ defer db.Close()
+ db.AutoMigrate(&Person{})
  r := gin.Default()
- r.GET("/", func(c *gin.Context) {
-  c.JSON(200, gin.H{
-   "message": "Hello world",
-  })
- })
- r.Run() 
+ r.GET("/", GetProjects)
+ r.Run(":8080")
+}
+func GetProjects(c *gin.Context) {
+ var people []Person
+ if err := db.Find(&people).Error; err != nil {
+    c.AbortWithStatus(404)
+    fmt.Println(err)
+ } else {
+    c.JSON(200, people)
+ }
 }
