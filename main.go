2a03e6084094ad458029f3c136c83daa96cf911c
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-08-12 02:03:41 +0530

    Adds update action

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
  r.GET("/people/", GetPeople)
  r.GET("/people/:id", GetPerson)
  r.POST("/people", CreatePerson)
+ r.PUT("/people/:id", UpdatePerson)
 
  r.Run(":8080")
 }
@@ -51,5 +52,16 @@ func CreatePerson(c *gin.Context) {
  var person Person
  c.BindJSON(&person)
  db.Create(&person)
+ c.JSON(200, person)
+}
+func UpdatePerson(c *gin.Context) {
+ var person Person
+ id := c.Params.ByName("id")
+ if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+    c.AbortWithStatus(404)
+    fmt.Println(err)
+ }
+ c.BindJSON(&person)
+ db.Save(&person)
  c.JSON(200, person)
 }
