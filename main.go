7df38f02c280d68db4d923fc58b846d1e918ec33
Pratik Kapasi <pratik.kapasi@razorpay.com>
2018-08-12 00:41:03 +0530

    Return a JSON response

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "github.com/gin-gonic/gin"
 func main() {
  r := gin.Default()
  r.GET("/", func(c *gin.Context) {
- c.String(200, "Hello World")
+  c.JSON(200, gin.H{
+   "message": "Hello world",
+  })
  })
  r.Run() 
 }
